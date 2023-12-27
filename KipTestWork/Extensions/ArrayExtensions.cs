151f6d3e2fce984e7217609149fd364ce7245193
GolubovskiyNI <nikitagolubovskiy@mail.ru>
2023-12-28 02:36:26 +0300

    Initialization

---
+++ KipTestWork/Extensions/ArrayExtensions.cs
@@ -0,0 +1,58 @@
+﻿namespace KipTestWork.Extensions;
+
+/// <summary>
+/// <see cref="Array"/> extensions implementation class.
+/// </summary>
+public static class ArrayExtensions
+{
+    /// <summary>
+    /// Transform the specified 2D array to array rows getting method.
+    /// </summary>
+    /// <typeparam name="TItemsType">Array items type.</typeparam>
+    /// <param name="array">Current array value.</param>
+    /// <returns>Array rows collection value.</returns>
+    public static IEnumerable<TItemsType[]> GetRows<TItemsType>(this TItemsType[,] array)
+    {
+        if (array is null)
+        {
+            throw new NullReferenceException();
+        }
+
+        var rowsAmount = array.GetLength(0);
+        var columnsAmount = array.GetLength(1);
+
+        if (rowsAmount <= 0 ||
+            columnsAmount <= 0
+        ) {
+            throw new ArgumentException();
+        }
+
+        for (var rowIndex = 0; rowIndex < rowsAmount; ++rowIndex)
+        {
+            var row = new TItemsType[columnsAmount];
+
+            for (var columnIndex = 0; columnIndex < columnsAmount; ++columnIndex)
+            {
+                row[columnIndex] = array[rowIndex, columnIndex];
+            }
+
+            yield return row;
+        }
+    }
+
+    /// <summary>
+    /// Returns a string that represents the current object.
+    /// </summary>
+    /// <typeparam name="TItemsType">Array items type.</typeparam>
+    /// <param name="array">Current array value.</param>
+    /// <returns>A string that represents the current object.</returns>
+    public static string GetItemsAsString<TItemsType>(this TItemsType[] array)
+    {
+        var result = array
+            .Aggregate(string.Empty, (current, item) =>
+                current + $"{item} "
+            );
+
+        return result;
+    }
+}
---
+++ KipTestWork/Program.cs
@@ -0,0 +1,57 @@
+﻿using KipTestWork.Extensions;
+
+var random = new Random();
+
+int[,] array;
+
+while (true)
+{
+    var rowsAmount = random.Next(2, 10);
+    var columnsAmount = random.Next(2, 10);
+
+    array = new int[rowsAmount, columnsAmount];
+
+    Console.WriteLine("Array:");
+
+    for (var rowIndex = 0; rowIndex < rowsAmount; ++rowIndex)
+    {
+        array[rowIndex, 0] = random.Next(0, 50);
+
+        Console.Write($"{array[rowIndex, 0]}");
+
+        for (var columnIndex = 1; columnIndex < columnsAmount; ++columnIndex)
+        {
+            array[rowIndex, columnIndex] = random.Next(0, 50);
+
+            Console.Write($", {array[rowIndex, columnIndex]}");
+        }
+
+        Console.WriteLine();
+    }
+
+    Console.WriteLine("Regenerate array?" + Environment.NewLine + "1 - True" + Environment.NewLine + "2 - False");
+
+    var regenerateArray = Console.ReadLine();
+
+    if (!int.TryParse(regenerateArray, out var regenerateArrayToInt))
+    {
+        throw new ArgumentException();
+    }
+
+    if (regenerateArrayToInt == 2)
+    {
+        break;
+    }
+}
+
+foreach (var arrayRow in array.GetRows())
+{
+    Console.WriteLine();
+    Console.WriteLine("Строка:");
+    Console.WriteLine(arrayRow.GetItemsAsString());
+    Console.WriteLine($"Максимальный элемент строки равен: {arrayRow.Max()}");
+    Console.WriteLine($"Минимальный элемент строки равен: {arrayRow.Min()}");
+    Console.WriteLine($"Cреднее арифметическое значение строки равно: {arrayRow.Average():F2}");
+}
+
+Console.ReadLine();
